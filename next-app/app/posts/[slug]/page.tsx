bc1f7dc8a80553b45d553aee508a196d55b8160d
ajbaker <37916428+alexbakertech@users.noreply.github.com>
2025-09-07 14:14:41 -0400

    Code cleanup

--- next-app/app/posts/[slug]/page.tsx
+++ next-app/app/posts/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import { prisma } from "@/lib/prisma"; //Had to add this to import a singleton instance of prisma client. This required me to install prisma and the @prisma/client package.
+import { Params } from "next/dist/server/request/params";
 
-export default async function PostPage({ params }) {
+export default async function PostPage({ params }: { params: Params }) {
 
     const post = await prisma.post.findUnique({
         where: {
